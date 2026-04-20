a392ed845813d7f15a59b66acd62a41a9e3bb12e
Winthak-orn <52524@rr.ac.th>
2026-04-20 14:23:15 +0700

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dekkaii-Web/', // สำคัญมาก: ตัวเล็กตัวใหญ่ต้องตรงกับชื่อ Repo บน GitHub
+  base: '/'
 })
